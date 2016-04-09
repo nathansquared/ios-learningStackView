e07a673d47ed9b49856218aa0a7d2d82e2136cc5
Nathan Adkins <nathanoadkins@gmail.com>
2016-04-09 15:31:26 -0700

    Initial Commit

---
+++ learningStackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learningStackView
+//
+//  Created by Nathan Adkins on 4/9/16.
+//  Copyright © 2016 Nathan Adkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
